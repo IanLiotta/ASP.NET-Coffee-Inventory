d494e0af403a3ea9c655a503597b76155f7d036a
Fiona Liotta <Fiona.Liotta@gmail.com>
2019-05-24 11:47:35 -0400

    Implemented green coffee roasting

--- SelectListTest/Controllers/InventoryController.cs
+++ SelectListTest/Controllers/InventoryController.cs
@@ -56,19 +56,7 @@ namespace SelectListTest.Controllers
         // GET: Inventory/Create
         public IActionResult Create()
         {
-            var coffees = _context.Coffees
-                .Select(s => new
-                {
-                    Id = s.Id,
-                    coffeeName = $"{s.Country.Name} {s.Variety.Name}",
-                })
-                .ToList();
-            InventoryViewModel ivm = new InventoryViewModel
-            {
-                Coffees = new SelectList(coffees, "Id", "coffeeName"),
-                Vendors = new SelectList(_context.Vendors.ToList(), "Id", "Name"),
-                Roasts = new SelectList(_context.Roasts.ToList(), "Id", "Name")
-            };
+            var ivm = GetInventoryViewModel();
             return View(ivm);
         }
 
@@ -180,6 +168,136 @@ namespace SelectListTest.Controllers
         private bool InventoryModelExists(int id)
         {
             return _context.Inventory.Any(e => e.Id == id);
+        }
+
+        private InventoryViewModel GetInventoryViewModel()
+        {
+            var coffees = _context.Coffees
+                .Select(s => new
+                {
+                    s.Id,
+                    coffeeName = $"{s.Country.Name} {s.Variety.Name}",
+                })
+                .ToList();
+            InventoryViewModel ivm = new InventoryViewModel
+            {
+                Coffees = new SelectList(coffees, "Id", "coffeeName"),
+                Vendors = new SelectList(_context.Vendors.ToList(), "Id", "Name"),
+                Roasts = new SelectList(_context.Roasts.ToList(), "Id", "Name")
+            };
+            return ivm;
+        }
+
+        //GET: Inventory/GreenIndex
+        public async Task<ActionResult> GreenIndex()
+        {
+            var greenInventory = await _context.Inventory.Where(r => r.Roast.Name == "Green").Select(s => new InventoryViewModel
+            {
+                ItemId = s.Id,
+                CoffeeFullName = $"{s.Coffee.Country.Name} {s.Coffee.Variety.Name}",
+                VendorName = s.Vendor.Name,
+                RoastName = s.Roast.Name,
+                PricePerLbs = s.PricePerLbs,
+                LbsOnHand = s.LbsOnHand
+            })
+                .ToListAsync();
+            return View(greenInventory);
+        }
+
+        //GET: Inventory/Roast/5
+        [HttpGet]
+        public async Task<ActionResult> Roast(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            var roastMe = await _context.Inventory
+                .Include("Coffee")
+                .Include("Coffee.Country")
+                .Include("Coffee.Variety")
+                .Include("Vendor")
+                .Include("Roast")
+                .FirstOrDefaultAsync(i => i.Id == id);
+            if (roastMe == null)
+            {
+                return NotFound();
+            }
+            InventoryViewModel ivm = new InventoryViewModel {
+                ItemId = (int)id,
+                CoffeeId = roastMe.Coffee.Id,
+                CoffeeFullName = $"{roastMe.Coffee.Country.Name} {roastMe.Coffee.Variety.Name}",
+                VendorId = roastMe.Vendor.Id,
+                VendorName = roastMe.Vendor.Name,
+                RoastId = roastMe.Roast.Id,
+                RoastName = roastMe.Roast.Name,
+                LbsOnHand = roastMe.LbsOnHand,
+                PricePerLbs = roastMe.PricePerLbs,
+                Roasts = new SelectList(await _context.Roasts.ToListAsync(), "Id", "Name")
+            };
+            return View(ivm);
+        }
+
+        //POST: Inventory/Roast/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Roast(InventoryViewModel newRoast)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    //Find the source coffee we're roasting
+                    var source = await _context.Inventory.FindAsync(newRoast.ItemId);
+                    //Check that we have enough green coffee on hand to roast
+                    if(source.LbsOnHand >= newRoast.LbsOnHand)
+                    {
+                        source.LbsOnHand -= newRoast.LbsOnHand;
+                        //If we roasted all our green coffee, remove it from the inventory
+                        if(source.LbsOnHand <= 0)
+                        {
+                            _context.Remove(source.Id);
+                        }
+                        //Check if we have some of this roasted coffee already and update the amount if we do.
+                        var existingRoast = await _context.Inventory.FirstOrDefaultAsync(s => s.Coffee.Id == newRoast.CoffeeId && s.Roast.Id == newRoast.RoastId);
+                        if (existingRoast != null)
+                        {
+                            existingRoast.LbsOnHand += newRoast.LbsOnHand;
+                        }
+                        else
+                        {
+                            var addRoast = new InventoryModel
+                            {
+                                Coffee = await _context.Coffees.FindAsync(newRoast.CoffeeId),
+                                Vendor = await _context.Vendors.FindAsync(newRoast.VendorId),
+                                Roast = await _context.Roasts.FindAsync(newRoast.RoastId),
+                                LbsOnHand = newRoast.LbsOnHand,
+                                PricePerLbs = newRoast.PricePerLbs
+                            };
+                            _context.Inventory.Add(addRoast);
+                        }
+                        await _context.SaveChangesAsync();
+                    }
+                    else
+                    {
+                        ViewBag.ErrorMessage = "Tried to roast more coffee than you have!";
+                        return View(newRoast);
+                    }
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!InventoryModelExists(newRoast.ItemId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(newRoast);
         }
     }
 }
